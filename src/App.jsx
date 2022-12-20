0281a013ccd57c64db71ea4b5c5f25bdeb9b32af
JRoberto <kanon939@hotmail.com>
2022-12-20 00:11:52 -0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
+import axios from 'axios'
 import './App.css'
+
+
+
+const BASE_URL = "https://users-crud.academlo.tech/swagger/?format=openapi"
 
 function App() {
   
+  const getAllUsers = () =>{
+    const URL = `${BASE_URL}users/`
+    axios.get(URL)
+      .then (res => console.log (res.data))
+      .catch (err => console.log (err))
+  }
+
+  getAllUsers ()
 
   return (
     <div className="App">
