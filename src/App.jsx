93a52f719587310fa0dde20ab8a184be50f8b38d
JRoberto <kanon939@hotmail.com>
2022-12-20 22:37:25 -0700

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import axios from 'axios'
+import { useEffect } from 'react'
 import './App.css'
+import FormUsers from './components/FormUsers'
 
 
 
@@ -7,6 +9,7 @@ const BASE_URL = "https://users-crud.academlo.tech/swagger/?format=openapi"
 
 function App() {
   
+  // funcion que obtine todos los usuarios
   const getAllUsers = () =>{
     const URL = `${BASE_URL}users/`
     axios.get(URL)
@@ -14,11 +17,32 @@ function App() {
       .catch (err => console.log (err))
   }
 
-  getAllUsers ()
+
+  //funcion para crear un usuario
+  const createUser = () => {
+    const URL = `${BASE_URL}users/`
+    axios.post (URL, data)
+    .then (res=> console.log (res.data))
+    .catch(err=> console.log (err) )
+
+  }
+
+
+
+  //se obtienen todos los usuarios al cargar la aplicacion 
+  useEffect(() => {
+    getAllUsers ()
+
+  }, [])
+
+
 
   return (
     <div className="App">
+     
      <h1>Hola mundo</h1>
+     
+     <FormUsers createUser={createUser}/>
     </div>
   )
 }
